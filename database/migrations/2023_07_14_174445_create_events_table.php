a767e3a978d5b7d5cbd16082d379821675ab4174
ikoma <maimai_na_srtci@yahoo.co.jp>
2023-07-17 11:06:42 +0900

    eventテーブル名変更

--- database/migrations/2023_07_14_174445_create_events_table.php
+++ database/migrations/2023_07_14_174445_create_events_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('=events', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->date('startDay');
             $table->date('endDay');
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('=events');
+        Schema::dropIfExists('events');
     }
 };
