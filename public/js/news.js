f854cfa011c3994e05153ffc0350a56f6f540d78
ikoma <maimai_na_srtci@yahoo.co.jp>
2023-07-23 09:08:05 +0900

    各果物等のお知らせにクリックすると閉じる開くを繰り返せるように編集

--- public/js/news.js
+++ public/js/news.js
@@ -13,3 +13,33 @@ document.getElementById("article-news").addEventListener("click", () => {
   
   document.getElementById("newscherrycoment").style.display = "none";
 });
+
+// タケノコ販売お知らせ
+document.getElementById("newsbamboocoment").style.display = "none";
+
+const bambooComent = document.getElementById("newsbamboo");
+bambooComent.addEventListener("click", (event) => {
+  event.stopPropagation();
+  
+  document.getElementById("newsbamboocoment").style.display = "";
+});
+
+document.getElementById("article-news2").addEventListener("click", () => {
+  
+  document.getElementById("newsbamboocoment").style.display = "none";
+});
+
+// レモンのお知らせ
+document.getElementById("newslemoncoment").style.display = "none";
+
+const lemonComent = document.getElementById("newslemon");
+lemonComent.addEventListener("click", (event) => {
+  event.stopPropagation();
+  
+  document.getElementById("newslemoncoment").style.display = "";
+});
+
+document.getElementById("article-news3").addEventListener("click", () => {
+  
+  document.getElementById("newslemoncoment").style.display = "none";
+});
--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -6,9 +6,9 @@
 @endsection
 @section('content')
 
-
+<section class="news">
 <article id="article-news">
-  <section class="news">
+  
     <h2>お知らせ</h2>
     <br>
     <dl>
@@ -16,18 +16,26 @@
       <dd id="newscherry">サクランボ狩りの予約を開始しました。</dd>
       <div id="newscherrycoment">大変お待たせ致しました。 <br>初夏の旬の味のサクランボが今年も立派に育ちました。<br><a href="{{ route('posts.harvest') }}">果物狩り【予約】</a> へご予約をお待ちしております。</div>
     </dl>
+    </article>
+
+    <article id="article-news2">
     <dl>
       <dt>2023.3.25</dt>
       <dd id="newsbamboo">たけのこ期間限定販売</dd>
       <div id="newsbamboocoment">今年は期間限定で、果物狩りに来園いただきましたお客様限定でタケノコの販売をさせていただきました。 <br>ご好評につき、完売いたしました。ありがとうございます。 <br>次の販売は来年の2024年2～4月ごろの予定になります。</div>
     </dl>
+    </article>
+
+
+    <article id="article-news3">
     <dl>
       <dt>2023.1.23</dt>
       <dd id="newslemon">レモン狩り終了のお知らせ</dd>
       <div id="newslemoncoment">今年のレモン狩りは終了いたしました。 <br>次のレモン狩りは来年2024年10月～1月ごろの予定になります。 <br>グリーンレモンという希少のレモンについてもお知らせに掲載いたします。 <br>さまざまな果物たちと同様に楽しみにお待ちください。 </div>
     </dl>
-  </section>
-</article>
+  
+  </article>
+</section>
 @endsection
 
 @section('footer')
