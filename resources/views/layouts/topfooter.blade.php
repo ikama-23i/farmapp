61c9f0575bcefb23e42bd907ca4ceaeb48d8f75e
ikoma <maimai_na_srtci@yahoo.co.jp>
2023-08-11 20:49:15 +0900

    index表示内容、装飾を編集

--- resources/views/layouts/topfooter.blade.php
+++ resources/views/layouts/topfooter.blade.php
@@ -5,7 +5,7 @@
       <p><strong>農業組合法人 Ikoma農園</strong></p>
       <p>〒496-0851 <br>愛知県津島市新明町1 </p>
       <p>TEL <br>0567-26-3216</p>
-      <p>火曜~日曜 <br>10:30-12:00 || 13:00-16:00</p>
+      <p>土曜日、日曜日 <br>10:30-13:00 || 13:00-115:30</p>
     </div>
     <p id="endc">&copy; 2023 Ikoma農園, All rights reserved.</p>
   </footer>
