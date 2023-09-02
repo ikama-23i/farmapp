ae314cd6ad588960d92039d257206b41da91a49b
ikoma <maimai_na_srtci@yahoo.co.jp>
2023-09-02 16:37:10 +0900

    文章追加

--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -7,10 +7,27 @@
 @section('content')
 
 <section class="news">
-<article id="article-news">
   
-    <h2>お知らせ</h2>
+<h2>お知らせ</h2>
     <br>
+
+    <article id="article-news4">
+    <dl>
+      <dt>2023.9.1</dt>
+      <dd id="newspersimmon">柿狩りの予約を開始しました。</dd>
+      <div id="newspersimmoncoment"> 大変お待たせ致しました。 <br>柿狩りの予約が可能になりました。今年も多く実っております。<br><a href="{{ route('posts.harvest') }}">果物狩り【予約】</a> へご予約をお待ちしております。</div>
+    </dl>
+    </article>
+
+    <article id="article-news5">   
+    <dl>
+      <dt>2023.8.31</dt>
+      <dd id="newscherryend">今年度（2023年）のサクランボ狩りは終了致しました。</dd>
+      <div id="newscherrycomentend">今年も多くのお客様にご利用いただき、ありがとうございました。<br>来年度（2024年）は6～7月の予定です。<br> </div>
+    </dl>
+    </article>
+
+    <article id="article-news">   
     <dl>
       <dt>2023.6.20</dt>
       <dd id="newscherry">サクランボ狩りの予約を開始しました。</dd>
@@ -33,8 +50,8 @@
       <dd id="newslemon">レモン狩り終了のお知らせ</dd>
       <div id="newslemoncoment">今年のレモン狩りは終了いたしました。 <br>次のレモン狩りは来年2024年10月～1月ごろの予定になります。 <br>グリーンレモンという希少のレモンについてもお知らせに掲載いたします。 <br>さまざまな果物たちと同様に楽しみにお待ちください。 </div>
     </dl>
-  
   </article>
+
 </section>
 @endsection
 
