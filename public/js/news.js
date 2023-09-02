22f61e162bc1d91970c96c18c67f8a1ba288ad55
ikoma <maimai_na_srtci@yahoo.co.jp>
2023-09-02 17:45:34 +0900

    装飾追加・JavaScriptで動作追加

--- public/js/news.js
+++ public/js/news.js
@@ -43,3 +43,33 @@ document.getElementById("article-news3").addEventListener("click", () => {
   
   document.getElementById("newslemoncoment").style.display = "none";
 });
+
+// サクランボ狩り終了お知らせ
+document.getElementById("newscherrycomentend").style.display = "none";
+
+const cherryComentend = document.getElementById("newscherryend");
+cherryComentend.addEventListener("click", (event) => {
+  event.stopPropagation();
+  
+  document.getElementById("newscherrycomentend").style.display = "";
+});
+
+document.getElementById("article-news5").addEventListener("click", () => {
+  
+  document.getElementById("newscherrycomentend").style.display = "none";
+});
+
+// 柿狩りの予約開始お知らせ
+document.getElementById("newspersimmoncoment").style.display = "none";
+
+const persimmonComent = document.getElementById("newspersimmon");
+persimmonComent.addEventListener("click", (event) => {
+  event.stopPropagation();
+  
+  document.getElementById("newspersimmoncoment").style.display = "";
+});
+
+document.getElementById("article-news").addEventListener("click", () => {
+  
+  document.getElementById("newspersimmoncoment").style.display = "none";
+});
